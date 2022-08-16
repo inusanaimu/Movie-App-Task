022061e176c86174ce5fd5d1c7b0655b391ee89f
inusa naimu <inusanaimu@gmail.com>
2022-08-16 14:58:58 +0100

    App.js Basic Logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import React, { useState } from 'react'
+// import data from './data'
+
 import Header from './Components/Layout/Header'
 import SideNav from './Components/Layout/SideNav'
 import MainBody from './Components/Layout/MainBody'
-import data from './data'
+
+import MovieList from './Components/Logic/MovieList'
+import MoviePreview from './Components/Logic/MoviePreview'
 
 import './App.css'
 
 function App() {
+  const [movie, setMovie] = useState({})
+  const movieOnPreview = (item) => {
+    setMovie(item)
+  }
+
   return (
     <>
+      {/* {console.log(data)} */}
       <Header> Movie App</Header>
-      <SideNav> Movie App</SideNav>
-      <MainBody> Movie App</MainBody>
+      <SideNav>
+        <MovieList movieOnPreview={movieOnPreview} />
+      </SideNav>
+      <MainBody>
+        <MoviePreview movieOnPreview={movie} />
+      </MainBody>
     </>
   )
 }
---
+++ src/Components/Logic/MoviePreview.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MoviePreview = () => {
+  return <div></div>
+}
+
+export default MoviePreview
