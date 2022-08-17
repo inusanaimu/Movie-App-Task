4df75cb446981d39d71e6276774f836ada066732
inusa naimu <inusanaimu@gmail.com>
2022-08-17 02:03:53 +0100

    Every Logic Working

--- src/Components/Logic/MoviePreview.js
+++ src/Components/Logic/MoviePreview.js
@@ -4,6 +4,12 @@ const MoviePreview = ({ onPreview }) => {
   return (
     <div>
       <h1>{onPreview?.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w300${onPreview.backdrop_path}`}
+        alt={onPreview.title}
+        width={200}
+        height={200}
+      />
     </div>
   )
 }
