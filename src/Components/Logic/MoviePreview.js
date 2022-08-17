d1c243b63f5177633f50c781b88e74a0fb66872b
inusa naimu <inusanaimu@gmail.com>
2022-08-17 03:59:53 +0100

    Working On Styling

--- src/Components/Logic/MoviePreview.js
+++ src/Components/Logic/MoviePreview.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import Card from '../UI/Card'
+import BackDrop from '../UI/BackDrop'
 
 const MoviePreview = ({ onPreview }) => {
+  const {
+    title,
+    backdrop_path,
+    id,
+    original_language,
+    overview,
+    release_date,
+  } = onPreview
+
+  const url = `https://image.tmdb.org/t/p/w300${backdrop_path}`
+  const Image = () => {
+    return <img src={url} alt={title} />
+  }
+
   return (
     <div>
-      <h1>{onPreview?.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w300${onPreview.backdrop_path}`}
-        alt={onPreview.title}
-        width={200}
-        height={200}
-      />
+      <BackDrop url={url}>{/* <Image /> */}</BackDrop>
+      <Card>
+        <h1> {title}</h1>
+      </Card>
     </div>
   )
 }
---
+++ src/Components/UI/BackDrop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './BackDrop.module.css'
+
+const BackDrop = ({ url, children }) => {
+  return (
+    <div
+      className={classes.backDrop}
+      style={{ backgroundImage: `url(${url})` }}
+    >
+      {url}
+      {children}
+    </div>
+  )
+}
+
+export default BackDrop
